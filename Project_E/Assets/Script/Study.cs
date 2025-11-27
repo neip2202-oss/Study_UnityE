9dd746fe7d3ed41a7f4102d33a2d5d63aaf72b1d
neip2202-oss <neip2202@gmail.com>
2025-11-27 17:38:40 +0900

    251127 강의 최종

--- Project_E/Assets/Script/Study.cs
+++ Project_E/Assets/Script/Study.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Study : MonoBehaviour
 {
+    public string say = "";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log(this.say);
     }
 
     // Update is called once per frame
