4477a58c91e6f44875c933e65c5db72397d21f9e
neip2202-oss <neip2202@gmail.com>
2025-12-02 17:10:10 +0900

    251202 실습 최종
    팝업 ui 제작

---
+++ Project_E/Assets/Scripts/Spawn.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject targetPrefab;
+
+    public void SpawnTarget()
+    {
+        Instantiate(targetPrefab, transform.position, Quaternion.identity);
+    }
+
+}
